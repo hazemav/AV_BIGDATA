d0a8e0a87b1cc5a97b2a6b99336583a594191bf5
hazemav <hazemav@gmail.com>
2017-01-11 16:36:42 +0200

    Add files via upload

---
+++ AV_Master_Script.sh
@@ -0,0 +1,24 @@
+#Version 1.0  
+#
+#download scripts in path /home/EBDG/ecosys/scripts)
+#1) make sure you have an internet connection
+#2) run ip_config.sh         >> to configure your network and make ur ip static
+#3) make sure again from ur internet connection
+#4) run config_hostname.sh   >> to configure your hostname (EBDG)
+#5) run config_ssh.sh        >> to enable ssh connection
+#6) run download_ecosys.sh   >> it will download hadoop , hive and spark in path /home/EBDG/ecosys 
+#7) run config_bashrc.sh     >> this script export home directories for hadoop spark and hive
+#8) run config_hadoop.sh to configure hadoop
+#9) run config_hive.sh to configure hive
+#10) run config_spark.sh to configure spark
+#11) create desktop scripts to run the ecosystem.
+#
+# BY HAZEM AHMED  ( AV )
+# hazemav@gmail.com for any question
+# 11/01/2017
+########################################################### 
+
+mkdir -p /tmp/scripts
+
+wget 
+
